039c4148498f833dc75e9d6ce580ccc5d8fb71eb
Alex Kist <akist0707@gmail.com>
2023-11-09 13:40:56 -0600

    Complete day 1 of 30 Days of Python tutorial course

---
+++ day_1/helloworld.py
@@ -0,0 +1,50 @@
+# Import math library
+import math
+
+# EXERCISE: LEVEL 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+print('Alex')
+print('Kist')
+print("United States")
+print("I am enjoying 30 days of Python")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Alex'))
+print(type('Kist'))
+print(type("United States"))
+
+# EXERCISE: LEVEL 3
+integer = 10
+float = 10.3
+complex = 2 + 3j
+string = 'This is a string'
+boolean = True
+list = ['One', 'Two', 'Three']
+tuple = ('Four', 'Five', 'Six')
+set = {'Seven', 'Eight', 'Nine'}
+dictionary = {
+    'key': 'value'
+}
+
+print(integer)
+print(float)
+print(complex)
+print(string)
+print(boolean)
+print(list)
+print(tuple)
+print(set)
+print(dictionary)
+
+# Euclidean distance between (2,3) and (10,8)
+distance = math.sqrt((10 - 2) ** 2 + (8 - 3) ** 2)
+print(distance)
