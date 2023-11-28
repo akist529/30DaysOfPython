62b19bacd85bbfad34fb6dcf3770a54f9f61e001
Alex Kist <akist0707@gmail.com>
2023-11-27 22:08:46 -0600

    Complete day 15 of 30 Days of Python tutorial course

---
+++ day_15/python_type_errors.py
@@ -0,0 +1,36 @@
+# Day 15: 30 Days of Python Programming
+
+# EXERCISES: LEVEL 1
+
+# Example 1: SyntaxError
+print('Hello World')
+
+# Example 2: NameError
+age = 30
+print(age)
+
+# Example 3: IndexError
+numbers = [1,2,3,4,5]
+numbers[4]
+
+# Example 4: ModuleNotFoundError
+import math
+
+# Example 5: AttributeError
+math.pi
+
+# Example 6: KeyError
+users = { 'name': 'Alex', 'age': 30, 'country': 'USA' }
+users['country']
+
+# Example 7: TypeError
+4 + int('3')
+
+# Example 8: ImportError
+from math import pow
+
+# Example 9: ValueError
+int('12')
+
+# Example 10: ZeroDivisionError
+1/1
