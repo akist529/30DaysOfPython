d20be019f05f54fe006ac76e7ce22e5135e8f706
akist529 <akist0707@gmail.com>
2023-12-21 16:26:54 -0600

    Complete day 24 of 30 Days of Python tutorial course

---
+++ day_24/statistics.py
@@ -0,0 +1,8 @@
+# Day 24: 30 Days of Python Programming
+
+# To summarize, the main differences with python lists are:
+#   Arrays support vectorized operations, while lists don’t.
+#   Once an array is created, you cannot change its size. You will have to create a new array or overwrite the existing one.
+#   Every array has one and only one dtype. All items in it should be of that dtype.
+#   An equivalent numpy array occupies much less space than a python list of lists.
+#   numpy arrays support boolean indexing.
